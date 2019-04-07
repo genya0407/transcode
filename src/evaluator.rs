df513aa9cada21d8d77111f6d9b5fe08a3066af3
Yusuke Sangenya <longinus.eva@gmail.com>
2019-04-07 15:17:22 +0900

    tmp

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -14,14 +14,25 @@ impl Evaluator {
 
     pub fn eval(&mut self, pc: usize) {
         match self.context[pc].clone() {
-            TranscodeAST::Difference { left_pc, right_pc, .. } => {
-                self.eval(left_pc);
-                self.eval(right_pc);
-                let result = procedures::difference(
-                    self.context[left_pc].result_image(),
-                    self.context[right_pc].result_image()
-                ).unwrap();
-                self.context[pc] = TranscodeAST::Difference { left_pc: left_pc, right_pc: right_pc, result: Some(result) };
+            TranscodeAST::Difference { left_pc, right_pc, result } => {
+                if result.is_none() {
+                    self.eval(left_pc);
+                    self.eval(right_pc);
+                    let result = procedures::difference(
+                        self.context[left_pc].result_image(),
+                        self.context[right_pc].result_image()
+                    ).unwrap();
+                    self.context[pc] = TranscodeAST::Difference { left_pc: left_pc, right_pc: right_pc, result: Some(result) };
+                }
+            },
+            TranscodeAST::Threshold { target_pc, thresh, result } => {
+                if result.is_none() {
+                    self.eval(target_pc);
+                    self.context[pc] = TranscodeAST::Threshold {
+                        target_pc: target_pc, thresh: thresh,
+                        result: Some(procedures::threshold(self.context[target_pc].result_image(), thresh).unwrap())
+                    };
+                }
             },
             _ => ()
         }
